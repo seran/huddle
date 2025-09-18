35ecadadff2129416f618b193e418596368f0cc7
seran <7030273+seran@users.noreply.github.com>
2025-09-18 21:00:06 +0530

    basic models and controllers

---
+++ db/migrate/20250918152709_create_tasks.rb
@@ -0,0 +1,17 @@
+class CreateTasks < ActiveRecord::Migration[8.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.string :description
+
+      t.integer :state, default: 0
+      
+      t.boolean :archived, default: false
+      t.boolean :shelve, default: false
+
+      t.belongs_to :board
+      
+      t.timestamps
+    end
+  end
+end
